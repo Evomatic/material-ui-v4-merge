16aa67efdc91eb585ada62a9984fa09a62416b23
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-09-13 11:41:29 +0900

    added button component

--- src/components/Button/presets/0-default.jsx
+++ src/components/Button/presets/0-default.jsx
@@ -1,10 +1,16 @@
 import * as React from "react";
+
 import Button from "../Button";
 
 export default (
-  <Button
-    uxpId="button"
-    disabled={false}
-    label="Button">
+  <Button 
+    uxpId="action1" 
+    size="medium" 
+    variant="contained" 
+    color="primary"
+    aria-label="button"
+    >
+    Button
   </Button>
 );
+
